ba9f05d0e8fe4b26b056288f759e7d6b0e586856
user <ephrahim.lenaiayasa@student.moringaschool.com>
2023-06-12 00:18:00 +0300

    Completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    var messages = [];
+  
+    for (var i = 0; i < names.length; i++) {
+      var message = `Thank you, ${names[i]}, for attending the ${event}!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+  // Example usage
+  var names = ["Guadalupe", "Ollie", "Aki"];
+  var event = "birthday party";
+  var thankYouMessages = writeCards(names, event);
+  console.log(thankYouMessages);
